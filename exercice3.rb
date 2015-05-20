2a2e66d6428650bf622384f0cd6d46fecf4fd709
laurentdugauquier <laurent.dugauquier@sfr.fr>
2015-05-20 17:52:45 +0200

    exercice1 à 5

---
+++ exercice3.rb
@@ -0,0 +1,24 @@
+# count chickens 
+puts "I will now count my chickens:"
+# result 30 hens, 97 roosters
+puts " Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+# count eggs
+puts "Now I will count the eggs:"
+# result 7 eggs
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# true or false
+puts "Is it true that 3 + 2 < 5 - 7?"
+# calculator
+puts 3 + 2 < 5 - 7
+# calculator
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+# false
+puts "Oh, that's why it's false."
+# false or true
+puts "How about some more."
+# false or true
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
